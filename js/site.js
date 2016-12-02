a7d70dc9984e9939f23bb60eda866f3f8b4ecb19
Lusy Yurk <Lusy Yurk>
2016-12-02 19:37:31 +0200

    First commit for Airlines project

---
+++ js/site.js
@@ -0,0 +1,46 @@
+﻿
+(function () {
+
+    "use strict";
+
+    $(function () {
+        //Getting the weather every hour
+        setInterval(getWeather, 3600000);
+
+        //Getting the weather first time
+        getWeather();
+
+        function getWeather() {
+            var cities = ["barcelona", "london", "moscow", "newYork", "paris", "sydney", "telAviv", "tokio"];
+
+            cities.forEach(function (city) {
+                console.log(city);
+                $.ajax({
+
+                    // Verb: GET / POST
+                    type: "GET",
+
+                    // Address: 
+                    url: 'https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + city + '")&format=json',
+
+                    // Error - what to do: 
+                    error: function (err) {
+                        console.log("Error: " + err.status + ", " + err.statusText);
+                        $("#" + city).html("Data is unavailable.");
+                    },
+
+                    // Success - what to do: 
+                    success: function (result) {
+                        var temp = result.query.results.channel.item.condition.temp;
+                        var text = result.query.results.channel.item.condition.text;
+                        temp = parseInt(temp);
+
+                        temp = Math.round((temp - 32) * 5 / 9);
+                        console.log('city', city, 'temp', temp, 'text', text);
+                        $("#" + city).html("Temperature: " + temp + " °С " + "<br />" + text);
+                    }
+                });
+            });
+        }
+    });
+})();
---
+++ js/thanks.js
@@ -0,0 +1,52 @@
+﻿(function () {
+
+    "use strict"
+
+    $(document).ready(function () {
+        
+        //Filling details from URL
+        fillDetails();
+
+        function getParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
+
+        function fillDetails() {
+
+            //get fullName parametr from URL
+            var name = getParameterByName('fullName');
+
+            // get span element by ID
+            var detailsSpan = document.getElementById('detailsSpan');
+
+            //get date parametr from URL
+            var date = getParameterByName('date');
+
+            //get email parametr from URL
+            var email = getParameterByName('email');
+
+            //fill details into the span
+            detailsSpan.innerHTML = "  Passenger: " + name + "</br>" + "  Your flight Date: " + date + "</br>" + " Your Email: " + email + "<br />";
+
+        }
+        //Showing modal
+        $('#myModal').modal({
+            show: true
+        });
+    });
+})();
+
+
+
+
+
+
+
+
+
